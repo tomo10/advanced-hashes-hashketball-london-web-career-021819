8a58c6bbfb3fa804bf5c0d1daea29b4d2ed89079
Thomas Edwards <thce28@gmail.com>
2019-02-12 18:54:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -112,12 +112,12 @@ def big_shoe_rebounds
   
     team_data[:players].each do |player, player_data|
         array << player_data[:shoe]
-    end     
+         
         max = array.max
         puts max
         puts player
        
-         
+    end     
         
     
   end
