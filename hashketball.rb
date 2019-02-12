5759e1898a714f0768ec109cc788134e66811b25
Thomas Edwards <thce28@gmail.com>
2019-02-12 18:53:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -114,11 +114,11 @@ def big_shoe_rebounds
         array << player_data[:shoe]
     end     
         max = array.max
-        puts max 
-        if player[:shoe] == max
-          return player[:rebounds]  
+        puts max
+        puts player
+       
          
-        end
+        
     
   end
 end
